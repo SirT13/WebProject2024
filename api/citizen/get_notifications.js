474ba3897a053ef69edf28118b6d8824d84b83cd
d.baltimas <d.baltimas@fysikoaerioellados.gr>
2024-09-11 00:37:10 +0300

    changes in get_notifications api call

--- api/citizen/get_notifications.js
+++ api/citizen/get_notifications.js
@@ -1,13 +1,14 @@
 const db = require('../../utils/connect_db')
 
-exports.get_notifications = async(req,res,next)=>{
-    var sql = `SELECT n.created_at as created_at, n.is_active as is_active, i.name as name, i.quantity, c.name as category
+exports.get_notifications = async (req, res, next) => {
+    var sql = `SELECT n.id as notification_id,i.id as item_id, n.created_at as created_at, n.is_active as is_active, i.name as name, i.quantity, c.name as category
                 FROM notifications n
                 JOIN notifications_items ni ON n.id=ni.notification_id
                 JOIN items i ON i.id=ni.item_id
-                JOIN categories c ON i.category_id=c.id`;
-    db.query(sql,(err,results)=>{
-        if (err){
+                JOIN categories c ON i.category_id=c.id
+                order by n.id`;
+    db.query(sql, (err, results) => {
+        if (err) {
             throw err;
         }
         res.status(200).send(results)
