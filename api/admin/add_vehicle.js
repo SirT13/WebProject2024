c08e6efb6833e13f0c27f948289c321f82326afc
MikeTzanettis <mtzanettis@gmail.com>
2024-09-14 16:26:36 +0300

    changes

--- api/admin/add_vehicle.js
+++ api/admin/add_vehicle.js
@@ -2,22 +2,16 @@ const db = require('../../utils/connect_db')
 
 exports.add_vehicle = async(req,res,next)=>{
     var rescuer_id = req.body.rescuer_id
-    var sql1="SELECT username FROM users WHERE id = ?";
-    var sql2="INSERT INTO vehicles (username,rescuer_id,load) VALUES (?,?,?)";
-    db.query(sql1,[rescuer_id],function(err,res1){
+    var username = req.body.username
+
+    var sql="INSERT INTO vehicles (username,rescuer_id,load) VALUES (?,?,?)";
+    let username = res1[0].username
+    db.query(sql,[username,rescuer_id,''],function(err,res2){
+        
         if(err) {
             return next(err);
         };
-
-        let username = res1[0].username
-        db.query(sql2,[username,rescuer_id,''],function(err,res2){
-            
-            if(err) {
-                return next(err);
-            };
-            
-            res.status(200).send(JSON.stringify(res2));
-        })
-    })   
-
+        
+        res.status(200).send(JSON.stringify(res2));
+    })
 }
